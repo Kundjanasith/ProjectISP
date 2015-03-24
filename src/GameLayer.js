f8a5b4a6a2cf88177a9a138f19f5b6ad2102edd8
Kundjanasith <kundjanasith@gmail.com>
2015-03-24 15:29:16 +0700

    add StartScene.js
    sperate StartScene

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -26,12 +26,4 @@ var GameLayer = cc.LayerColor.extend({
 
 });
 
-var StartScene = cc.Scene.extend({
-    onEnter: function() {
-        this._super();
-        var layer = new GameLayer();
-        layer.init();
-        this.addChild( layer );
-    }
-});
 
---
+++ src/StartScene.js
@@ -0,0 +1,8 @@
+var StartScene = cc.Scene.extend({
+    onEnter: function() {
+        this._super();
+        var layer = new GameLayer();
+        layer.init();
+        this.addChild( layer );
+    }
+});
