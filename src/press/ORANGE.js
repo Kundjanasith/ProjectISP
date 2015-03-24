0e467038e694e66c4d90afbf81dfc4755173b277
Kundjanasith <kundjanasith@gmail.com>
2015-03-25 00:03:10 +0700

    add method change in ORANGE
    method change is called when ORANGE button is pressed down or up

--- src/press/ORANGE.js
+++ src/press/ORANGE.js
@@ -2,5 +2,13 @@
     ctor: function() {
         this._super();
         this.initWithFile( 'res/press/orange1.png' );
+    },
+    change: function(x){
+    	if(x=='up'){
+    		this.initWithFile( 'res/press/orange1.png' );
+    	}
+    	if(x=='down'){
+    		this.initWithFile( 'res/press/orange2.png' );
+    	}
     }
 }); 
