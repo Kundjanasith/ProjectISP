77036acea1cb15c26bf08dd643c8dcaadd230eee
Kundjanasith <kundjanasith@gmail.com>
2015-03-24 15:47:01 +0700

    add method change in red
    method change for change picture when it pressed

--- src/press/RED.js
+++ src/press/RED.js
@@ -2,5 +2,13 @@
      ctor: function() {
         this._super();
         this.initWithFile( 'res/press/red1.png' );
+    },
+    change: function(x){
+    	if(x=='up'){
+    		this.initWithFile( 'res/press/red1.png' );
+    	}
+    	if(x=='down'){
+    		this.initWithFile( 'res/press/red2.png' );
+    	}
     }
 }); 
