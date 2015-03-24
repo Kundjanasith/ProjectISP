d463e08a5604bfe2a6fdcbb3caa19205de6c28e6
Kundjanasith <kundjanasith@gmail.com>
2015-03-24 15:27:15 +0700

    can run StartScene

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ cc.game.onStart = function(){
     cc.view.setDesignResolutionSize(800, 450, cc.ResolutionPolicy.SHOW_ALL);
     cc.view.resizeWithBrowserSize(true);
     cc.LoaderScene.preload(g_resources, function () {
-        cc.director.runScene(new HelloWorldScene());
+        cc.director.runScene(new StartScene());
     }, this);
 };
 cc.game.run();
