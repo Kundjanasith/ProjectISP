bab5a89a757cae3b5236c0a9f0f7c3d586258ec3
Kundjanasith <kundjanasith@gmail.com>
2015-03-24 15:25:06 +0700

    add Basic method of GameLayer
    mostly method come from tutorial 100

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -1,8 +1,28 @@
-var res = {
+var GameLayer = cc.LayerColor.extend({
+    init: function() {
+        this._super( new cc.Color( 195, 195, 195, 0) );
+        this.setPosition( new cc.Point( 0, 0 ) );
+        
+        return true;
+    },
+    onKeyDown: function( e ) {
+   
+    },
+    onKeyUp: function( e ) {
 
-};
+    },
+    addKeyboardHandlers: function() {
+        var self = this;
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed : function( e ) {
+                self.onKeyDown( e );
+            },
+            onKeyReleased: function( e ) {
+                self.onKeyUp( e );
+            }
+        }, this);
+    }
 
-var g_resources = [];
-for (var i in res) {
-    g_resources.push(res[i]);
-}
+});
+
