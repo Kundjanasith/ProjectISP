7dfe195fc6033b5aa4e2a273ef388043b97c2951
Kundjanasith <kundjanasith@gmail.com>
2015-03-25 07:43:57 +0700

    add method change in YELLOW
    method change is called when YELLOW button is pressed down or up

--- src/press/YELLOW.js
+++ src/press/YELLOW.js
@@ -2,5 +2,13 @@
     ctor: function() {
         this._super();
         this.initWithFile( 'res/press/yellow1.png' );
+    },
+    change: function(x){
+        if(x=='up'){
+            this.initWithFile( 'res/press/yellow1.png' );
+        }
+        if(x=='down'){
+            this.initWithFile( 'res/press/yellow2.png' );
+        }
     }
 }); 
