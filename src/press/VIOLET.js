2db6925a7085d22a4a7f7d66c9b379c44df24dcd
Kundjanasith <kundjanasith@gmail.com>
2015-03-25 10:51:03 +0700

    add method change in VIOLET
    method change is called when VIOLET button is pressed down or up

--- src/press/VIOLET.js
+++ src/press/VIOLET.js
@@ -2,5 +2,13 @@
     ctor: function() {
         this._super();
         this.initWithFile( 'res/press/violet1.png' );
+    },
+    change: function(x){
+    	if(x=='up'){
+    		this.initWithFile( 'res/press/violet1.png' );
+    	}
+    	if(x=='down'){
+    		this.initWithFile( 'res/press/violet2.png' );
+    	}
     }
 }); 
