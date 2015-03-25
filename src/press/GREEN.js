ca151a0bd79dfb709bd82d2a5d0f6a4d9aae709b
Kundjanasith <kundjanasith@gmail.com>
2015-03-25 08:32:38 +0700

    add method change in GREEN
    method change is called when GREEN button is pressed down or up

--- src/press/GREEN.js
+++ src/press/GREEN.js
@@ -2,5 +2,13 @@
     ctor: function() {
         this._super();
         this.initWithFile( 'res/press/green1.png' );
+    },
+    change: function(x){
+    	if(x=='up'){
+    		this.initWithFile( 'res/press/green1.png' );
+    	}
+    	if(x=='down'){
+    		this.initWithFile( 'res/press/green2.png' );
+    	}
     }
 }); 
