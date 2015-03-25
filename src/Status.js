12642be2aac27f6b46811d740a8451d3668621ce
Kundjanasith <kundjanasith@gmail.com>
2015-03-25 15:04:01 +0700

    add method change in status
    PLAY && STOP

--- src/Status.js
+++ src/Status.js
@@ -2,5 +2,13 @@
     ctor: function() {
         this._super();
         this.initWithFile( 'res/status/Start.png' );
+    },
+    change: function(x){
+    	if(x=='play'){
+    		this.initWithFile( 'res/status/Play.png' );
+    	}
+    	if(x=='stop'){
+    		this.initWithFile( 'res/status/Stop.png' );
+    	}
     }
 }); 
