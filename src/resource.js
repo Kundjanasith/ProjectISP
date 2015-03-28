027370ba1f3f55a435621089aaf5b901dd4d23f9
Kundjanasith <kundjanasith@gmail.com>
2015-03-28 18:14:52 +0700

    create resource Screen_png

--- src/resource.js
+++ src/resource.js
@@ -1,6 +1,7 @@
 var res = {
     StartNormal_png : "res/start_game0.png",
-    StartSelected_png : "res/start_game1.png"
+    StartSelected_png : "res/start_game1.png",
+    Screen_png : "res/galaxy.png"
 };
 
 var g_resources = [];
